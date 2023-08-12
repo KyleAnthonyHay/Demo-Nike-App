14519dee5e039c2c04e3984086c5b7cacbc58037
KyleAnthonyHay <haykyle917@gmail.com>
2023-08-12 10:42:50 -0500

    search bar added

--- nikeapp/lib/pages/shop_page.dart
+++ nikeapp/lib/pages/shop_page.dart
@@ -10,8 +10,28 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Shop'),
+    return Column(
+      children: [
+        //search bar
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search'),
+              Icon(Icons.search),
+            ],
+          ),
+        )
+        //message
+
+        //hot picks
+      ],
     );
   }
 }
